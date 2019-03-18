8f33498cb2fe480ff5b494dd9fd355afe3cab526
joelazar <jozsef.gabor.lazar@gmail.com>
2019-03-18 17:28:08 +0100

    Space Age - small correction based on review

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,20 +1,23 @@
 package space
 
 import "math"
+import "time"
 
 type Planet string
 
+var earth_year time.Duration = 31557600 * time.Second
+
 var planets = map[Planet]float64{
-	"Earth":   31557600,
-	"Mercury": 0.2408467 * 31557600,
-	"Venus":   0.61519726 * 31557600,
-	"Mars":    1.8808158 * 31557600,
-	"Jupiter": 11.862615 * 31557600,
-	"Saturn":  29.447498 * 31557600,
-	"Uranus":  84.016846 * 31557600,
-	"Neptune": 164.79132 * 31557600,
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
 func Age(seconds float64, planet Planet) float64 {
-	return math.Round((seconds/planets[planet])/0.01) * 0.01
+	return math.Round((seconds/(planets[planet]*earth_year.Seconds()))/0.01) * 0.01
 }
