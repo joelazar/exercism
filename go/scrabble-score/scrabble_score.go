b8de8df5719a5ab2122b4758db1db55a8bf40f4d
joelazar <jozsef.gabor.lazar@gmail.com>
2019-09-07 21:39:21 +0200

    scrabble_score corrected based on review

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,43 +1,28 @@
+// This package contains scrabble related functions.
 package scrabble
 
-import "strings"
-
-var scrabbleScores = map[rune]int{
-	'a': 1,
-	'e': 1,
-	'i': 1,
-	'o': 1,
-	'u': 1,
-	'l': 1,
-	'n': 1,
-	'r': 1,
-	's': 1,
-	't': 1,
-	'd': 2,
-	'g': 2,
-	'b': 3,
-	'c': 3,
-	'm': 3,
-	'p': 3,
-	'f': 4,
-	'h': 4,
-	'v': 4,
-	'w': 4,
-	'y': 4,
-	'k': 5,
-	'j': 8,
-	'x': 8,
-	'q': 10,
-	'z': 10,
-}
+import "unicode"
 
 // Score - this function calculates the score of the given Scrabble word
 func Score(word string) int {
 	score := 0
-	word = strings.ToLower(word)
-
 	for _, char := range word {
-		score += scrabbleScores[char]
+		switch unicode.ToLower(char) {
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		default:
+			score++
+		}
 	}
 	return score
 }
