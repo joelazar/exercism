2b5e5b99f0911433c4d8c2c3a41a8944b9634342
joelazar <jozsef.gabor.lazar@gmail.com>
2019-11-06 21:18:04 +0100

    much faster solution for isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,21 +1,17 @@
 package isogram
 
-import "sort"
 import "strings"
 import "unicode"
 
 // IsIsogram - this function checks if a word is isogram or not
 func IsIsogram(input string) bool {
-	runeList := []rune(strings.ToLower(input))
-	sort.Slice(runeList, func(i, j int) bool {
-		return runeList[i] < runeList[j]
-	})
+	input = strings.ToLower(input)
 
-	for i := 0; i < len(runeList)-1; i++ {
-		if !unicode.IsLetter(runeList[i]) {
+	for i, char := range input {
+		if !unicode.IsLetter(char) {
 			continue
 		}
-		if runeList[i] == runeList[i+1] {
+		if strings.Contains(input[i+1:], string(char)) {
 			return false
 		}
 	}
