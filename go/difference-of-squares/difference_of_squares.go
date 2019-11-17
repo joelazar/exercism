1414003bd6fd113e972e4188679cc1ebb5c30e05
joelazar <jozsef.gabor.lazar@gmail.com>
2019-11-17 19:54:10 +0100

    two more exercises are ready

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,19 @@
 package diffsquares
+
+import "math"
+
+// SquareOfSum - calculate the square of the sum of the first n natural numbers
+func SquareOfSum(n int) int {
+	return int(math.Pow(float64((n*(n+1))/2), 2))
+}
+
+// SumOfSquares - calculate the sum of the first squared n natural numbers
+func SumOfSquares(n int) int {
+	return ((n * (n + 1) * (2*n + 1)) / 6)
+}
+
+// Difference - calculate the difference between SquareOfSum and SumOfSquares
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+
+}
--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,1 +1,16 @@
 package pangram
+
+import "strings"
+
+const abc = "abcdefghijklmnopqrstunwxyz"
+
+func IsPangram(input string) bool {
+	input = strings.ToLower(input)
+	for _, r := range abc {
+		if !strings.Contains(input, string(r)) {
+			return false
+		}
+	}
+
+	return true
+}
