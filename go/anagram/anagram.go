5236d272f0643baaea64c556cd9131e34b3f5f73
joelazar <jozsef.gabor.lazar@gmail.com>
2019-11-18 21:36:06 +0100

    anagram is ready

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,1 +1,25 @@
 package anagram
+
+import "sort"
+import "strings"
+
+// sortString - sorts a string into an alphabetical order
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+// Detect - this functions detects anagrams of given string from an array of strings and returns with them
+func Detect(subject string, candidates []string) []string {
+	anagrams := []string{}
+	subject = strings.ToLower(subject)
+	sortedSubject := sortString(subject)
+
+	for _, candidate := range candidates {
+		if subject != strings.ToLower(candidate) && sortedSubject == sortString(strings.ToLower(candidate)) {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+	return anagrams
+}
