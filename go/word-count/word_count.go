93c2c8072a62901a09cae2d68a790f5f4635a57d
joelazar <jozsef.gabor.lazar@gmail.com>
2019-11-24 18:11:36 +0100

    word_count ready

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,1 +1,33 @@
 package wordcount
+
+import "strings"
+import "unicode"
+
+type Frequency map[string]int // Using this return type.
+
+var freqMap Frequency
+
+func f(c rune) bool {
+	return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '\''
+}
+
+func filterApostrophe(s string) string {
+	if strings.IndexAny(s, "'") == 0 {
+		s = s[1:]
+	}
+	if strings.IndexAny(s, "'") == len(s)-1 {
+		s = s[:len(s)-1]
+	}
+	return s
+}
+
+func WordCount(sentence string) Frequency {
+	freqMap = make(Frequency)
+	words := strings.FieldsFunc(sentence, f)
+	for _, word := range words {
+		word = filterApostrophe(strings.ToLower(word))
+		freqMap[word] += 1
+	}
+
+	return freqMap
+}
