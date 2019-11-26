a88bf751224bcabb41dc7c2698240ae601fdf2ee
joelazar <jozsef.gabor.lazar@gmail.com>
2019-11-26 20:32:42 +0100

    without math

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,10 +1,8 @@
 package diffsquares
 
-import "math"
-
 // SquareOfSum - calculate the square of the sum of the first n natural numbers
 func SquareOfSum(n int) int {
-	return int(math.Pow(float64((n*(n+1))/2), 2))
+	return (n * (n + 1) / 2) * (n * (n + 1) / 2)
 }
 
 // SumOfSquares - calculate the sum of the first squared n natural numbers
