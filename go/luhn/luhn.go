1358ea4a09cfacbc2047bfb85fdd1e6cc06c8f44
joelazar <jozsef.gabor.lazar@gmail.com>
2020-04-14 16:23:39 +0200

    two exercises solved for go path

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,1 +1,32 @@
 package luhn
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Valid(input string) bool {
+	input = strings.Replace(input, " ", "", -1)
+
+	if len(input) < 2 {
+		return false
+	}
+
+	sum := 0
+	mod := len(input) % 2
+
+	for i, char := range input {
+		number, err := strconv.Atoi(string(char))
+		if err != nil {
+			return false
+		}
+		if i%2 == mod {
+			number *= 2
+			if number > 9 {
+				number -= 9
+			}
+		}
+		sum += number
+	}
+	return (sum % 10) == 0
+}
--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,1 +1,40 @@
 package prime
+
+var primes []int
+
+func IsPrime(num int) bool {
+	for _, prime := range primes {
+		if prime == 0 {
+			return true
+		}
+		if num%prime == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func Nth(n int) (int, bool) {
+	if n < 1 {
+		return 0, false
+	}
+	counter := 1
+	number := 2
+	primes = make([]int, n)
+	primes[0] = 2
+
+	if n == 1 {
+		return primes[0], true
+	}
+
+	for {
+		if IsPrime(number) {
+			primes[counter] = number
+			counter += 1
+			if counter == n {
+				return number, true
+			}
+		}
+		number += 1
+	}
+}
