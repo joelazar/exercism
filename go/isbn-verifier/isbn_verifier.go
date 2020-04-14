db85225bcf7c6168ae7847f32865581e5f9f39e6
joelazar <jozsef.gabor.lazar@gmail.com>
2020-04-14 17:27:56 +0200

    and two more

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,1 +1,26 @@
 package isbn
+
+import (
+	"strconv"
+	"strings"
+)
+
+func IsValidISBN(input string) bool {
+	sum := 0
+	input = strings.Replace(input, "-", "", -1)
+	if len(input) != 10 {
+		return false
+	}
+	for i, char := range input {
+		num, err := strconv.Atoi(string(char))
+		if err != nil {
+			if i != len(input)-1 || char != 'X' {
+				return false
+			}
+			num = 10
+		}
+		sum += num * (10 - i)
+	}
+
+	return (sum % 11) == 0
+}
--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,1 +1,42 @@
 package lsproduct
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func NProduct(input []int, span int) int {
+	prod := 1
+	for i := 0; i < span; i++ {
+		prod *= input[i]
+	}
+	return prod
+}
+
+func LargestSeriesProduct(input string, span int) (int, error) {
+	numinput := []int{}
+	for _, char := range input {
+		num, err := strconv.Atoi(string(char))
+		if err != nil {
+			return 0, fmt.Errorf("array contains non-digit: %v", err)
+		}
+		numinput = append(numinput, num)
+	}
+
+	if span > len(numinput) {
+		return 0, fmt.Errorf("span must be smaller than the array")
+	}
+	if span < 0 {
+		return 0, fmt.Errorf("span must be non negative")
+	}
+
+	maxprod := NProduct(numinput, span)
+	for i := 1; i < len(numinput)-(span-1); i++ {
+		newprod := NProduct(numinput[i:], span)
+		if maxprod < newprod {
+			maxprod = newprod
+		}
+	}
+
+	return maxprod, nil
+}
